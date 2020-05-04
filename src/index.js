c24a8c2f48c746ad127f985a5f953e5f43178b90
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-05-04 17:16:57 +0300

    first commit, working on button onChange

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+
+function JokeLogic() {
+    const [data, setData] = useState({});
+    // const [joke, setJoke] = useState({value : ''});
+
+
+    useEffect(() => {
+        axios.get('https://api.chucknorris.io/jokes/random')
+            .then(response => {
+                console.log(response);
+                setData(response.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
+
+    return (
+        <div className="joke_logic">
+            <div className="select_option">
+                <input type="radio" /><label>Random</label> <br/>
+                <input type="radio" /><label>From Categories</label> <br/>
+                <input type="radio" /><label>Search</label> <br/>
+                <button type="button" className="btn" onClick={() => setData}>Get a joke</button>
+            </div>
+            <div className="joke_itself">
+                <div className="joke">{data.value}</div>
+            </div>
+        </div>
+    );
+}
+
+function Sidebar() {
+    return (
+        <aside className="sidebar">
+            <div>Favorite jokes</div>
+        </aside>
+    )
+}
+
+function Content() {
+    return (
+        <div className="content">
+            <div className="intro">
+                <h3>MSI 2020</h3>
+                <h1>Hey!</h1>
+                <h2>Let's try to find a joke for you:</h2>
+            </div>
+                <JokeLogic />
+        </div>
+    )
+}
+
+function MainContent() {
+        return (
+            <section className="main_content">
+                <Content/>
+                <Sidebar/>
+            </section>
+        )
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <MainContent />,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
