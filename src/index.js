ed53bdf32189194ff3569d598766969c39116eda
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-05-04 19:04:55 +0300

    19:04 04/05/20 finished button onChange, start working on radio buttons.

--- src/index.js
+++ src/index.js
@@ -5,27 +5,62 @@ import axios from 'axios'
 
 function JokeLogic() {
     const [data, setData] = useState({});
-    // const [joke, setJoke] = useState({value : ''});
+    const [count, setCount] = useState(0);
 
 
     useEffect(() => {
         axios.get('https://api.chucknorris.io/jokes/random')
             .then(response => {
                 console.log(response);
-                setData(response.data)
+                setData(response.data);
             })
             .catch(err => {
                 console.log(err);
             })
-    }, []);
+    }, [count]);
 
     return (
         <div className="joke_logic">
             <div className="select_option">
-                <input type="radio" /><label>Random</label> <br/>
-                <input type="radio" /><label>From Categories</label> <br/>
-                <input type="radio" /><label>Search</label> <br/>
-                <button type="button" className="btn" onClick={() => setData}>Get a joke</button>
+                <form>
+                    <div className="form-check">
+                        <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="option1"
+                                defaultChecked={true}
+                                className="form-check-input"
+                            />
+                            Random
+                        </label>
+                    </div>
+
+                    <div className="form-check">
+                        <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="option2"
+                                className="form-check-input"
+                            />
+                            From Categories
+                        </label>
+                    </div>
+
+                    <div className="form-check">
+                        <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="option3"
+                                className="form-check-input"
+                            />
+                            Search
+                        </label>
+                    </div>
+                    <button type="button" className="btn" onClick={() => setCount(count + 1)}>Get a joke</button>
+                </form>
             </div>
             <div className="joke_itself">
                 <div className="joke">{data.value}</div>
